1c790fcaf0ad6b7f124bc69ac1330bc8f00627cb
Taxusorg <taxus@taxus.org>
2017-12-03 22:32:18 +0800

    repository add find

--- src/Repository/RoleRepository.php
+++ src/Repository/RoleRepository.php
@@ -6,11 +6,28 @@ use Taxusorg\Permission\Contracts\RoleRepository as RepositoryContracts;
 
 class RoleRepository implements RepositoryContracts
 {
-    public function getById($id)
+    /**
+     * @param $id
+     * @return RoleResourceModel
+     */
+    public function find($id)
     {
         return RoleResourceModel::find($id);
     }
 
+    /**
+     * @param int $id
+     * @return RoleResourceModel
+     */
+    public function getById($id)
+    {
+        return $this->find($id);
+    }
+
+    /**
+     * @param string $name
+     * @return mixed
+     */
     public function getByName($name)
     {
         return RoleResourceModel::where('name', $name)->first();
